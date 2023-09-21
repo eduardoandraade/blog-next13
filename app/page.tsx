27a91b01580e7f8c12a5894008fef269d5d9c33e
Eduardo Andrade <109284750+eduardoandraade@users.noreply.github.com>
2023-09-21 08:00:05 -0300

    feat(deploy): Adiciona alterações para implantação
    Apenas para o cliente ver as ultimas alterações

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import Stats from "./components/Stats"
 import Services from "./components/Services"
 import About from "./components/About";
 import Testimonials from "./components/Testimonials";
+import {RecentsBlogs} from "./components/RecentsBlogs";
+import PostsBlogs from "./components/PostsBlogs";
 
 
 export default async function IndexPage(){
@@ -15,6 +17,8 @@ export default async function IndexPage(){
         <Services />
         <About />
         <Testimonials />
+        <RecentsBlogs />
+        <PostsBlogs />
 
     </>
 
